38ab7841203a119bae7c480d8191c44cbda3c1c9
udeos <ruzhitskiy.bogdan@gmail.com>
2016-02-23 18:47:52 +0200

    simple decision tree classifier

--- DecisionTreeClassifier.py
+++ DecisionTreeClassifier.py
@@ -6,16 +6,21 @@ from sklearn.base import ClassifierMixin, BaseEstimator
 
 
 class BTree(object):
-    def __init__(self, predicate, left, right, params):
+    label = None
+
+    def __init__(self, proportions, predicate=None, left=None, right=None):
         self.predicate = predicate
         self.left, self.right = left, right
-        self.params = params
+        self.proportions = proportions
+        if predicate is None:
+            self.label = max(proportions, key=proportions.get)
 
     def __str__(self):
         descr = []
         if self.predicate:
             descr.append('X[%s] <= %s' % (self.predicate[0], round(self.predicate[1], 4)))
-        descr.extend(['%s = %s' % (k, v) for k, v in self.params.items()])
+        if self.label:
+            descr.append('Lb = %s' % self.label)
         return '\n'.join(descr)
 
     def to_graphviz(self):
@@ -36,68 +41,82 @@ class BTree(object):
 
 
 class DecisionTreeClassifier(ClassifierMixin, BaseEstimator):
+
     def __init__(self):
         self.tree = None
+        self.n_features = None
 
-    _entropy_vec = np.vectorize(lambda i, s: (i / float(s)) ** 2,
-                                otypes=[np.float])
+    def _is_valid(self):
+        if self.tree is None:
+            raise Exception("Tree is not built yet")
 
-    def get_entropy(self, Y):
-        _, counts = np.unique(Y, return_counts=True)
-        return 1 - self._entropy_vec(counts, float(Y.shape[0])).sum()
+    @staticmethod
+    def _get_proportions(Y):
+        keys, counts = np.unique(Y, return_counts=True)
+        return dict(zip(keys, counts / float(len(Y))))
 
-    def fit(self, X, Y):
+    def _build(self, D):
+        kwargs = {
+            'proportions': self._get_proportions(D['label'].flatten()),
+        }
+        predicate = self._find_predicate(D)
+        if predicate:
+            f_id, f_val = predicate
+            mask = D[str(f_id)] <= f_val
+            kwargs['predicate'] = predicate
+            kwargs['left'] = self._build(D[mask])
+            kwargs['right'] = self._build(D[~mask])
+        return BTree(**kwargs)
 
-        def build(XX, YY):
-            params = {'entropy': self.get_entropy(YY)}
-            left = right = None
-            predicate = self.find_predicate(XX, YY)
-            if predicate:
-                f_id, f_val = predicate
-                mask = XX[:, f_id] <= f_val
-                left = build(XX[mask], YY[mask])
-                right = build(XX[~mask], YY[~mask])
-            else:
-                params['class'] = YY[0]
-            return BTree(predicate, left, right, params)
-
-        self.tree = build(X, Y)
-
-    def find_predicate(self, X, Y):
-        min_entropy = self.get_entropy(Y)
-        if min_entropy == 0.:
+    def _find_predicate(self, D):
+        No = float(D.shape[0])
+        Nf = self.n_features
+        uniques, counts = np.unique(D['label'], return_counts=True)
+        min_impurity = 1 - sum([(i / No) ** 2 for i in counts])
+        if min_impurity == 0.:
             return None
         predicate = None
-        n_Y = len(Y)
-        for obj in X:
-            for f_id, f_val in enumerate(obj):
-                mask = X[:, f_id] <= f_val
-                y1, y2 = Y[mask], Y[~mask]
-                n_y1, n_y2 = len(y1), len(y2)
-                if n_y2 == 0 or n_y2 == 0:
+        for fid in range(Nf):
+            left = dict.fromkeys(uniques, 0)
+            right = dict(zip(uniques, counts))
+            Dp = np.sort(D[[str(fid), 'label']], order=str(fid), axis=0)
+            for label, objs in itertools.groupby(Dp.flatten(), lambda x: x[1]):
+                objs = list(objs)
+                left[label] += len(objs)
+                right[label] -= len(objs)
+                left_values, right_values = left.values(), right.values()
+                left_num, right_num = float(sum(left_values)), float(sum(right_values))
+                if right_num == 0:
                     continue
-                e1 = self.get_entropy(y1) * n_y1
-                e2 = self.get_entropy(y2) * n_y2
-                local_entropy = (e1 + e2) / n_Y
-                if local_entropy < min_entropy:
-                    min_entropy = local_entropy
-                    predicate = f_id, f_val
+                left_impurity = 1 - sum([(i / left_num) ** 2 for i in left_values])
+                right_impurity = 1 - sum([(i / right_num) ** 2 for i in right_values])
+                current_impurity = (left_impurity * left_num + right_impurity * right_num) / No
+                if current_impurity < min_impurity:
+                    min_impurity = current_impurity
+                    predicate = fid, objs.pop()[0]
         return predicate
+
+    def fit(self, X, Y):
+        self.n_features = X.shape[1]
+        D = np.concatenate((X, Y.reshape(Y.shape[0], 1)), axis=1)
+        dt = [(str(i), 'f8') for i in range(self.n_features)]
+        dt.append(('label', 'f8'))
+        D = D.view(dt)
+        self.tree = self._build(D)
 
     def predict(self, X):
 
         def go_deep(tree, obj):
             if tree.predicate is None:
-                return tree.params['class']
+                return tree.label
             f_id, f_val = tree.predicate
             if obj[f_id] <= f_val:
                 return go_deep(tree.left, obj)
             return go_deep(tree.right, obj)
 
-        leafs = np.array([go_deep(self.tree, obj) for obj in X])
-        return leafs
+        self._is_valid()
+        return np.array([go_deep(self.tree, obj) for obj in X])
 
     def draw_tree(self, filename):
-        if self.tree is None:
-            raise Exception("Tree is not built yet")
+        self._is_valid()
         self.tree.to_graphviz().write_png(filename)
--- DecisionTreeClassifier_usage.py
+++ DecisionTreeClassifier_usage.py
@@ -1,11 +1,8 @@
 from sklearn import datasets, cross_validation
-
 from DecisionTreeClassifier import DecisionTreeClassifier
 
 
-iris = datasets.load_iris()
-X, Y = iris.data, iris.target
+X, Y = datasets.make_classification(n_samples=1000, n_features=10)
 clf = DecisionTreeClassifier()
 clf.fit(X, Y)
 print cross_validation.cross_val_score(clf, X, Y)
-clf.draw_tree('DecisionTreeClassifier_visual.png')
